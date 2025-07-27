e7ec8c4d6392f01dcbdaa8409018fa7c74a63ddd
suttirakcha <suttirak.char@gmail.com>
2025-07-27 11:09:11 +0700

    fix: change type of id of getDealById from number to string

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,32 +1,34 @@
-import prisma from "../config/prisma.js"
+import prisma from "../config/prisma.js";
 
-export const checkRole = (user_role, statusCode, errorMessage) => (req, res) => {
-  const { role } = req.user;
-  if (role !== user_role){
-    createErrorUtil(statusCode, errorMessage);
-  }
-}
+export const checkRole =
+  (user_role, statusCode, errorMessage) => (req, res) => {
+    const { role } = req.user;
+    if (role !== user_role) {
+      createErrorUtil(statusCode, errorMessage);
+    }
+  };
 
-export const getAllDeal = async() => {
+export const getAllDeal = async () => {
   const result = await prisma.deal.findMany({
-    include :{
-      category : true,
-      seller : true,
-      images :  true,
-    }
-  })
-  return result
-}
-export const getDealById = async(data) => {
-  const {id} = data
-  const result = await prisma.deal.findFirst({
-    where : {
-      id : Number(id)
+    include: {
+      category: true,
+      seller: true,
+      images: true,
     },
-    include : {
-      seller : true,
-      category : true,
-      images : true,
-    }
-  })
-}
+  });
+  return result;
+};
+
+export const getDealById = async (id) => {
+  const result = await prisma.deal.findFirst({
+    where: {
+      id,
+    },
+    include: {
+      seller: true,
+      category: true,
+      images: true,
+    },
+  });
+  return result;
+};
