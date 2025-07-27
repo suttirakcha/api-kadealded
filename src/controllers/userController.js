9f6f1af4c5235f5bddcf8ea8a20f3a61a509392d
suttirakcha <suttirak.char@gmail.com>
2025-07-27 11:08:44 +0700

    fix: insert params of getDealById controller

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -3,16 +3,16 @@ import { getAllDeal, getDealById } from "../services/user.service.js";
 export const controllerGetAll = async (req, res, next) => {
   try {
     const result = await getAllDeal();
-    res.status(200).json({ result });
+    res.status(200).json({ message: "Deals found", result });
   } catch (error) {
     console.log(error);
     next(error);
   }
 };
-export const controllerGetAllDealById = async() => {
+export const controllerGetAllDealById = async(req, res, next) => {
   try {
-    const result = await getDealById(req.params)
-    res.status(200).json({result})
+    const result = await getDealById(req.params.id)
+    res.status(200).json({ message: "Deal found", result })
   } catch (error) {
     console.log(error)
     next(error)
