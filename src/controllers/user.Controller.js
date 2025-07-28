4c9cd8abf7a7bb7de745e959f76ce885b496549b
jobtnbd17 <jobtnbd@gmail.com>
2025-07-28 15:07:24 +0700

    Fix: tel_number Int > String

---
+++ src/controllers/user.Controller.js
@@ -0,0 +1,34 @@
+import {
+  getAllDeal,
+  getDealById,
+  getProfile,
+} from "../services/user.service.js";
+
+export const controllerGetAll = async (req, res, next) => {
+  try {
+    const result = await getAllDeal();
+    res.status(200).json({ message: "Deals found", result });
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+};
+export const controllerGetAllDealById = async (req, res, next) => {
+  try {
+    const result = await getDealById(req.params.id);
+    res.status(200).json({ message: "Deal found", result });
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+};
+export const controllerGetProfile = async (req, res, next) => {
+  try {
+    const result = await getProfile(req.params.id);
+    res.status(200).json({ result });
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+};
+
--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,9 +1,11 @@
 import express from "express";
-import { controllerGetAll, controllerGetAllDealById } from "../controllers/userController.js";
+import { controllerGetAll, controllerGetAllDealById, controllerGetProfile } from "../controllers/user.Controller.js";
+import { authUserCheck } from "../middlewares/auth.middleware.js";
 
 const userRouter = express.Router();
 
 userRouter.get("/deal",controllerGetAll);
 userRouter.get("/deal/:id",controllerGetAllDealById);
+userRouter.get("/auth/profile",authUserCheck,controllerGetProfile)
 
 export default userRouter;
