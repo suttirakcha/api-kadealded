f727c3d225945bea9d998ec1c454f5771353ae26
suttirakcha <suttirak.char@gmail.com>
2025-07-28 17:59:37 +0700

    feat: set success message for login controller

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,21 +1,21 @@
 import { login, registerService } from "../services/auth.service.js";
 
-export const registerController =  async (req, res, next) => {
+export const registerController = async (req, res, next) => {
   try {
-    const result = await registerService(req.body)
-    res.status(201).json({message : `Register ${result.name} successful`})
+    const result = await registerService(req.body);
+    res.status(201).json({ message: `Register ${result.name} successful` });
   } catch (error) {
     console.log(error);
     next(error);
   }
 };
 
-export const loginController = async(req,res,next) => {
+export const loginController = async (req, res, next) => {
   try {
-    const result = await login(req.body)
-   res.status(201).json(result)
+    const result = await login(req.body);
+    res.status(201).json({ message: "Successfully logged in", ...result });
   } catch (error) {
-    console.log(error)
-    next(error)
+    console.log(error);
+    next(error);
   }
-}
+};
