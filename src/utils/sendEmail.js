d43a6bedf6f554acb16729fa8836cbc32b509cf5
Lordalpaca <althanida@gmail.com>
2025-08-03 14:53:57 +0700

    feat: finish otp registration

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -1,5 +1,9 @@
 import nodemailer from "nodemailer";
 
+console.log("DEBUG CHECK: EMAIL_USER loaded:", process.env.EMAIL_USER);
+console.log("DEBUG CHECK: EMAIL_PASS loaded (first 5 chars):", process.env.EMAIL_PASS ? process.env.EMAIL_PASS.substring(0, 5) + '...' : 'PASSWORD NOT LOADED');
+// This will show us if the correct values are being read.
+
 export const sendEmail = async ({ to, subject, html }) => {
   const transporter = nodemailer.createTransport({
     service: "gmail",
