9c496d7e627fb81167e628ace3d9b23bfd8ca067
suttirakcha <suttirak.char@gmail.com>
2025-08-04 17:46:13 +0700

    fix: logout route

--- src/routers/authUserRouter.js
+++ src/routers/authUserRouter.js
@@ -1,24 +1,11 @@
 import express from "express";
-<<<<<<< Updated upstream
-import {
-  loginController,
-  registerController,
-} from "../controllers/auth.controller.js";
-import {
-  loginSchema,
-  registerSchema,
-  validate,
-} from "../validators/validation.js";
-=======
 import { loginController, logoutController, registerController } from "../controllers/auth.controller.js";
 import { loginSchema, registerSchema, validate } from "../validators/validation.js";
->>>>>>> Stashed changes
 import { loginWithGoogle } from "../controllers/google-auth.controller.js";
 import { getAllDealJoiner } from "../controllers/admin.controller.js";
 import uploadMiddleware from "../middlewares/upload.middleware.js";
 
 const authUserRouter = express.Router();
-<<<<<<< Updated upstream
 authUserRouter.post(
   "/register",
   uploadMiddleware.array("profile_image", 5),
@@ -26,13 +13,8 @@ authUserRouter.post(
   registerController
 );
 authUserRouter.post("/login", validate(loginSchema), loginController);
-authUserRouter.post("/auth/google", loginWithGoogle);
-=======
-authUserRouter.post('/register',validate(registerSchema),registerController)
-authUserRouter.post('/login',validate(loginSchema),loginController);
 authUserRouter.post('/logout',logoutController);
-authUserRouter.post('/auth/google', loginWithGoogle);
->>>>>>> Stashed changes
+authUserRouter.post("/auth/google", loginWithGoogle);
 authUserRouter.get("/deals/:id/joiners", getAllDealJoiner);
 
 export default authUserRouter;
